500c05d80d6e6fc509940441ab724b2e2fc527a9
Paul Gusmano <155201655+pm-gusmano@users.noreply.github.com>
2025-07-17 14:17:37 +0000

    implement rescale function

--- src/example_package_pm_gusmano/rescale.py
+++ src/example_package_pm_gusmano/rescale.py
@@ -1,2 +1,9 @@
-def rescale():
-    print("Hi!")
+import numpy as np
+
+def rescale(input_array):
+    """Rescale an input array to the range [0, 1]."""
+
+    low = np.min(input_array)
+    high = np.max(input_array)
+    output_array = (input_array - low) / (high - low)
+    return output_array
